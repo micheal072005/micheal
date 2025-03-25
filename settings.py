1f1dc6a5e7667163f9ead1458fc2b70ee77e0671
Micheal <sonmikeagudozie@gmail.com>
2025-03-25 09:50:48 +0100

    Update email settings to use SSL instead of TLS

--- settings.py
+++ settings.py
@@ -25,8 +25,8 @@ load_dotenv()
 
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")  # Fetch from .env
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")  # Fetch from .env
 
